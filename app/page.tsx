a0e3bd6c1f35a2162d8fe118843393eb36355048
zakky <fakhrimuzakki06@gmail.com>
2026-01-30 14:27:18 +0700

    update: cache the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import HomeClient from "./HomeClient";
 import { getData } from "@/libs/api";
 import type { APIResponse } from "@/types";
 
+export const revalidate = 30;
+
 export default async function Home() {
   const apiUrl = process.env.API_URL;
   const data = await getData<APIResponse>(`${apiUrl}/products`);
